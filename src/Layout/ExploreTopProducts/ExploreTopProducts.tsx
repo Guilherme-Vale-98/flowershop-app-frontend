563676011a2064efc8cc1febb007a8e44aad9bfb
Guilherme Vale <guilherme.vale98@gmail.com>
2023-10-20 01:08:59 -0300

    add new font

--- src/Layout/ExploreTopProducts/ExploreTopProducts.tsx
+++ src/Layout/ExploreTopProducts/ExploreTopProducts.tsx
@@ -9,10 +9,10 @@ const ExploreTopProducts = (props: Props) => {
       <div>
         <img src="/images/flowerHeader.jpg" alt="" />
       </div>
-      <div className=" p-5 text-white m-5 card-img-overlay">
-        <p className=" card-text">Desabroche seus momentos</p>
-        <h1 className=" card-title w-50">A arte da natureza nos seus melhores momentos</h1>
-        <p className="card-text">"Cultivado com amor, escolhidas a dedo com muito carinho".</p>
+      <div className="fs-5 p-5 text-white m-5 card-img-overlay">
+        <p className=" fs-4 mt-2 card-text">Desabroche seus momentos</p>
+        <h1 className="fs-1 card-title w-50">A arte da natureza nos seus melhores momentos</h1>
+        <p className=" card-text">"Cultivado com amor, escolhidas a dedo com muito carinho".</p>
       </div>
     </div>
   )
--- src/Layout/Navbar/Navbar.tsx
+++ src/Layout/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = (props: Props) => {
   return (
 <nav className="navbar fs-4 purple navbar-expand-lg navbar-light">
   <div className="text-white container-fluid">
-    <a className="navbar-brand text-white ms-5 ps-5" href="#">FLAUER</a>
+    <a className="navbar-brand text-white fs-2 ms-5 ps-5" href="#">Blooms Shop</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -16,19 +16,19 @@ const Navbar = (props: Props) => {
           <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-white" href="#">Shop</a>
+          <a className="nav-link  text-white" href="#">Shop</a>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link text-white dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Products
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Buquês</a></li>
+            <li><a className=" fs-5 dropdown-item" href="#">Buquês</a></li>
           </ul>
         </li>
       </ul>
       <form className="d-flex">
-        <button className="btn btn-outline-success" type="submit">Sign in</button>
+        <button className="btn fs-4 btn-outline-success" type="submit">Sign in</button>
       </form>
     </div>
   </div>
