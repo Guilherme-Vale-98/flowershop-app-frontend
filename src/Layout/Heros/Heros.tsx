a584269c1c22eeb89b979b516ff377d0742ba080
Guilherme Vale <guilherme.vale98@gmail.com>
2023-10-26 04:50:49 -0300

    Added Product Model

--- src/Layout/Heros/Heros.tsx
+++ src/Layout/Heros/Heros.tsx
@@ -4,15 +4,15 @@ type Props = {}
 
 const Heros = (props: Props) => {
     return (
-        <div className="p-4 flower-bg" style={{backgroundColor: '#FFFFFF'}} >
+        <div className=" flower-bg" style={{backgroundColor: '#FFFFFF'}} >
             <div className="container ">
                 <div className="row align-items-center no-gutters">
                     <div className="col-xl-5 col-lg-6 col-md-12">
-                        <div className="py-2  py-lg-0">
+                        <div className=" py-lg-0">
                             <p className=''>Sobre nós</p>
                             <h1 className="display-5 fw-bold">Conheça a My Flower Shop
                             </h1>
-                            <p className=" mb-4 lead">
+                            <p className=" mb-2 lead">
                                 Hand-picked Instructor and expertly crafted courses, designed for the modern students and entrepreneur.
                             </p>
                             <a href="" className="btn btn-lg btn-success">Saiba mais</a>
---
+++ src/models/ProductModel.ts
@@ -0,0 +1,23 @@
+class ProductModel {
+    id: Number;
+    name: String;
+    description?:String;
+    copies?: Number;
+    copiesAvailable?: Number;
+    category?: String;
+    img?: String;
+
+
+    constructor(id: Number, name: String, description: String, copies: Number, copiesAvailable: Number, category: String, img: String){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.copies = copies;
+        this.copiesAvailable = copiesAvailable;
+        this.category = category;
+        this.img = img;
+
+    }
+
+
+}
